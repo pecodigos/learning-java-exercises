938d58ca969f1ea8b5f8e90bfd03978f58bf6fde
pecodigos <pedrenriquecunha@gmail.com>
2024-07-20 00:51:22 -0300

    polymorphism

--- 14-inheritance-and-polymorphism/14-inheritance-and-polymorphism01/src/application/Program.java
+++ 14-inheritance-and-polymorphism/14-inheritance-and-polymorphism01/src/application/Program.java
@@ -1,9 +1,19 @@
 package application;
 
+import entities.Account;
+import entities.SavingsAccount;
+
 public class Program
 {
     public static void main(String[] args)
     {
+        Account x = new Account(1020, "Pedro", 1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
 
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
     }
 }
