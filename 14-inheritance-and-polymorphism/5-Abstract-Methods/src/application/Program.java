0ece1f17ca5ab85b9c1743cc3c026b248f350f1b
pecodigos <pedrenriquecunha@gmail.com>
2024-07-20 16:49:52 -0300

    abstract methods

--- 14-inheritance-and-polymorphism/5-Abstract-Methods/src/application/Program.java
+++ 14-inheritance-and-polymorphism/5-Abstract-Methods/src/application/Program.java
@@ -1,9 +1,59 @@
 package application;
 
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
+import entities.enums.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program
 {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter the number of shapes: ");
+        int n = sc.nextInt();
+
+        List<Shape> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++)
+        {
+            System.out.printf("\nShape #%d data:\n", i + 1);
+            System.out.print("Rectangle or Circle (r/c)? ");
+            char ch = sc.next().charAt(0);
+
+            System.out.print("Color (BLACK/BLUE/RED): ");
+            Color color = Color.valueOf(sc.next());
+
+            if (ch == 'r')
+            {
+                System.out.print("Width: ");
+                double width = sc.nextDouble();
+
+                System.out.print("Height: ");
+                double height = sc.nextDouble();
+
+                list.add(new Rectangle(color, height, width));
+            }
+            else
+            {
+                System.out.print("Radius: ");
+                double radius = sc.nextDouble();
+
+                list.add(new Circle(color, radius));
+            }
+        }
+
+        System.out.println("\nSHAPE OF AREAS:");
+
+        for (Shape shape : list)
+        {
+            System.out.printf("%.2f\n", shape.area());
+        }
+        sc.close();
     }
 }
