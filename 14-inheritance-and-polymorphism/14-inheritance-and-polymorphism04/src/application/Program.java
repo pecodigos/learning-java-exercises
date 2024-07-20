5e2d00a26bf3ddb354e5d3adf11b75fd1a4c75b3
pecodigos <pedrenriquecunha@gmail.com>
2024-07-20 15:24:20 -0300

    abstract Class

---
+++ 14-inheritance-and-polymorphism/14-inheritance-and-polymorphism04/src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program
+{
+    public static void main(String[] args)
+    {
+        List<Account> list = new ArrayList<>();
+
+        list.add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+        list.add(new BusinessAccount(1002, "Maria", 1000.00, 400.00));
+        list.add(new SavingsAccount(1004, "Bob", 300.00, 0.01));
+        list.add(new BusinessAccount(1005, "Ana",500.00, 500.0));
+
+        double sum = 0.0;
+
+        for (Account acc : list)
+        {
+            sum += acc.getBalance();
+        }
+        System.out.printf("Total balance: $%.2f\n", sum);
+
+        for (Account acc : list)
+        {
+            acc.deposit(10.0);
+        }
+
+        for (Account acc : list)
+        {
+            System.out.printf("Updated Balance for %s's account: $%.2f\n", acc.getHolder(), acc.getBalance());
+        }
+    }
+}
