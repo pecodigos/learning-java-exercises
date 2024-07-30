534efb92e793c26e25c0addc4cc3786de11563e0
pecodigos <pedrenriquecunha@gmail.com>
2024-07-30 18:32:29 -0300

    declared lambda expression

--- 19-Functional-Programming-and-Lambda/01-Functional-Programming-Lambda-Calc/src/application/Program.java
+++ 19-Functional-Programming-and-Lambda/01-Functional-Programming-Lambda-Calc/src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
 import entities.Product;
-import util.ProductPredicate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Program {
 
@@ -17,7 +17,11 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(Product::nonStaticProductPredicate);
+        double min = 100.0;
+
+        Predicate<Product> predicate = p -> p.getPrice() >= min;
+
+        list.removeIf(predicate);
 
         for (Product p : list) {
             System.out.println(p);
