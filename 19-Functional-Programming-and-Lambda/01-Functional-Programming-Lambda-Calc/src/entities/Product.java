fbb294572928eeca33a38c8b146fa9be4d4786d5
pecodigos <pedrenriquecunha@gmail.com>
2024-07-30 18:30:01 -0300

    Non static reference method

--- 19-Functional-Programming-and-Lambda/01-Functional-Programming-Lambda-Calc/src/entities/Product.java
+++ 19-Functional-Programming-and-Lambda/01-Functional-Programming-Lambda-Calc/src/entities/Product.java
@@ -30,6 +30,10 @@ public class Product {
         return p.getPrice() >= 100.00;
     }
 
+    public boolean nonStaticProductPredicate() {
+        return price >= 100.00;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
