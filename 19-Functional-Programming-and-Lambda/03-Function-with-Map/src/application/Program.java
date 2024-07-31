962eda4c836be91c1621c6f5b62deaf3d6b0f600
pecodigos <pedrenriquecunha@gmail.com>
2024-07-31 02:29:01 -0300

    Static reference method

--- 19-Functional-Programming-and-Lambda/03-Function-with-Map/src/application/Program.java
+++ 19-Functional-Programming-and-Lambda/03-Function-with-Map/src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import entities.Product;
-import util.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +17,7 @@ public class Program {
         list.add(new Product("Table", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        List <String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+        List <String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
 
         names.forEach(System.out::println);
     }
