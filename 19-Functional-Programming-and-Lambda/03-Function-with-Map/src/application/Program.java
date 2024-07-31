21e083c03f304f0a146f3d87a9640108afd52ad0
pecodigos <pedrenriquecunha@gmail.com>
2024-07-31 02:31:45 -0300

    Declared lambda expression

--- 19-Functional-Programming-and-Lambda/03-Function-with-Map/src/application/Program.java
+++ 19-Functional-Programming-and-Lambda/03-Function-with-Map/src/application/Program.java
@@ -4,6 +4,7 @@ import entities.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Program {
@@ -17,7 +18,9 @@ public class Program {
         list.add(new Product("Table", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        List <String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+        Function<Product, String> func = p -> p.getName().toUpperCase();
+
+        List <String> names = list.stream().map(func).collect(Collectors.toList());
 
         names.forEach(System.out::println);
     }
